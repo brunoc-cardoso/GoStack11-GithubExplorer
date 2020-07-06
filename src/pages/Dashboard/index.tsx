07e473d6265853529830486f3f3815dec0f7a877
bruno.cardoso <brunocpessoal@gmail.com>
2020-07-05 22:02:33 -0300

    add styling to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img src="https://avatars2.githubusercontent.com/u/33553892?s=460&v=4" alt="Bruno Cardoso"/>
+          <div>
+            <strong>brunoc-cardoso/SemanaOmnistack</strong>
+            <p>Apenas uma descrição de teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 }
 
 export default Dashboard;
